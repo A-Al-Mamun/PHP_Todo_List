094cfcf13fbd60446ffb83ccc1cb79de5b7c30e6
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2024-01-07 23:38:58 +0600

    New repo of Todo list by using PHP Programming Language

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP To Do List Project | Created By A-Al-Mamun</title>
+
+    <!-- style Link -->
+    <link rel="stylesheet" href="./assets/style.css">
+</head>
+<body>
+    <h1>To Do List Project</h1>
+    <script src="./assets/main.js"></script>
+</body>
+</html>
